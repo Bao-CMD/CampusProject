5628ba07801826abdbe02d1938ad91f52ada21c4
fungusmash <48535037+fungusmash@users.noreply.github.com>
2019-12-03 09:18:44 -0700

    Add files via upload

--- Campus.hpp
+++ Campus.hpp
@@ -7,28 +7,41 @@
 
 using namespace std;
 
-struct vertex;
-
 struct node {
   string name;
-  int distance;
+  int distanceX;
+  int distanceY;
 };
 
+struct vertex;
+
 struct adjvertex {
+  vertex* v;
+};
+
+struct vertex{
   string name;
   bool visited = false;
-  int distance = 0;
+  float distance = 0;
   vertex *pred = NULL;
   vector<adjvertex> adj;
 };
 
 class graph {
-public:
+  public:
   void addEdge(string v1, string v2);
   void addVertex(string v);
-  void printInfo();
-private:
+  void DijkstraAlgorithm(string start, string end);
+  private:
   vector<vertex*> vertices;
 };
+
+float distanceY (float y1, float y2);
+float distanceX (float x1, float x2);
+float diagonalDistance (float x, float y);
+float worstCaseDistance(float x, float y);
+float averageCaseDistance(float best, float worst);
+float distanceToTime (float distance);
+
 
 #endif
